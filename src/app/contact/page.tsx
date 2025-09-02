30a50eb7459743a36e62a1bfa482c7dd2460f40e
Bruno <bruno-lopes9@hotmail.com>
2025-09-02 14:17:28 +0100

    fix import path

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import { FaEnvelope, FaPhone, FaMapMarkerAlt } from 'react-icons/fa'
 import { motion } from 'framer-motion'
-import { fadeInUp, fadeIn, slideInLeft, slideInRight } from '@/utils/animations'
+import { fadeInUp, fadeIn, slideInLeft, slideInRight } from '../utils/animations'
 
 interface FormData {
   name: string;
