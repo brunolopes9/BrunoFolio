e75ee09be6356df841c347a2ff7ec628d7b03671
Bruno <bruno-lopes9@hotmail.com>
2025-09-02 14:09:32 +0100

    add routes to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Blogs from "./components/Blogs";
+import Hero from "./components/Hero";
+import Newsletter from "./components/Newsletter";
+import Projects from "./components/Projects";
+
 
 export default function Home() {
   return (
-<div> 
-
-</div>
+    <main>
+      <Hero />
+      <Projects />  
+      <Blogs />
+      <Newsletter />
+    </main>
   );
-}
+} 
